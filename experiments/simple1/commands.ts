e961e9df0b3df58be069fa10efcce508caaee565
Brad Olson <brad.olson@movedbylight.com>
2024-09-18 01:12:32 -0400

    wip: add notes

--- experiments/simple1/commands.ts
+++ experiments/simple1/commands.ts
@@ -13,6 +13,10 @@ interface Pargs<F> {
  * And add methods to build the command. A checker can be added for tests
  * to walk the tree and check for discrepancies, I don't think we
  * make flags hierarchical we could easily switch it I suppose.
+ * Typebox and Zod facades can be added
+ * I'll start with a light no deps parser.
+ * typings for multicommands are a little unclear, they might need an
+ * expanded interface, with getSubcommand<F>(args) => {tail, command<F>}
  */
 export interface Command<Flags> {
   slug: () => string;
@@ -22,19 +26,21 @@ export interface Command<Flags> {
   execute: (args: Pargs<Flags>) => Promise<number>; // exit status
 
   // utility mothods
+  output: (text: string) => Promise<void>;
   flagNames: () => string[]; // for sanity checks
   // deno-lint-ignore no-explicit-any
-  subcommands: () => Command<any>[]; // to walk tree for sanity checks
+  subcommands: () => Command<any>[]; // to walk tree for sanity checks, if this is a leaf commanad, this is an empty array
 }
 
-// deno-lint-ignore no-explicit-any
 export async function run<T>(cmd: Command<T>, args: string[]) {
   try {
     const pargs = cmd.parse(args);
     const exitCode = await cmd.execute(pargs);
     // warn if exit code? No function should except
-    return exitCode;
+    return Deno.exit(exitCode);
   } catch (err) {
-    console.error(err.message);
+    // await cmd.output(cmd.help())
+    console.error("ERROR", err.message);
+    console.error(`Run '${cmd.slug()} --help' for more information.`);
   }
 }
