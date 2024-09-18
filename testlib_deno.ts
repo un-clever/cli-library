af4c3cf55bc29ea8a74c9d99c04eb8d35ca34d0a
Brad Olson <brad.olson@movedbylight.com>
2024-09-18 11:27:54 -0400

    test: restructure testing imports...
    In an attempt to make this library testable cross-platform,
    I moved the deno test imports to ./testlib_deno.ts
    And exposed it via the import Map.

--- testlib_deno.ts
+++ testlib_deno.ts
@@ -8,3 +8,12 @@ export { describe, it } from "@std/testing/bdd";
  */
 export { assert, assertEquals } from "@std/assert";
 export { assertType } from "@std/testing/types";
+
+// seems *almost* compatible with node's testing
+// as of 18 Sep 2024 defined in
+// https://github.com/denoland/deno/cli/js/40_test.js#L338
+// and assigned to globalThis.Deno.test at the bottom of the file
+// looking at the code, I'd avoid it. It'd be hard to facade into
+// something Bun or Node compatible I think, and it's not really
+// exposed in any public library.
+export const globalTest = Deno.test;
