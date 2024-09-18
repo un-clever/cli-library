2d1381f6307a81a08fe6a17d4150cfd720e31dab
Brad Olson <brad.olson@movedbylight.com>
2024-09-18 18:09:22 -0400

    feat: strongly type FlagSpec and utilities...
    Also added utilities to extract the structure that would result from parsing a
    given flagspec

--- experiments/simple1/flags.ts
+++ experiments/simple1/flags.ts
@@ -25,11 +25,45 @@ export interface Flag<T> {
   parser: FlagParser<T>;
 }
 
-// export const dashdash: Flag<string[]> = {
-//   description: "ignore params after --",
-//   slugs: ["--"],
-//   shortcuts: [],
-//   required: false,
-//   default: [],
-//   parser: (args: string[]) => ({ value: args, tail: [] }),
-// };
+/**
+ * A utility type to grab the type from the flag
+ *
+ * For those new to this format, see Typescript docs on conditional types.
+ * To unpack the semantics:
+ * 1. To get TypeFromFlag from any type
+ * 2. If that type extends Flag, grab (infer) the type of flag it is.
+ * 3. And if it doesn't extend Flag it's an error (should never happen)
+ */
+export type TypeFromFlag<F> = F extends Flag<infer T> ? T : never;
+
+/**
+ * A utility type to grab the type from a flag parser
+ */
+export type TypeFromFlagParser<F> = F extends FlagParser<infer T> ? T : never;
+
+/**
+ * The common interface for building sets of flags for commands will be a
+ * dictionary of Flag objects. This is the structure, but you probably don't
+ * want to type those constants because this type tag will tend to de-specify
+ * the types.
+ *
+ * Instead, write the flag spec as a constant, then use the FlagsType utility
+ * below to extract the types
+ */
+// deno-lint-ignore no-explicit-any
+export type UntypedFlagSpec = Record<string, Flag<any>>;
+
+/**
+ * If you want to explicitly type the flag spec, you could do it. See example in the tests file.
+ */
+export type FlagSpec<F> = {
+  [K in keyof F]: K extends string ? Flag<F[K]> : never;
+};
+
+/**
+ * A utility to infer the type of a group of parsed flags resulting from a
+ * applying a flag spec to args.
+ */
+export type FlagsType<FS> = {
+  [K in keyof FS]: FS[K] extends Flag<infer T> ? T : never;
+};
--- experiments/simple1/parseArgs.test.ts
+++ experiments/simple1/parseArgs.test.ts
@@ -1,41 +1,5 @@
-import { assertEquals, assertType, describe, it } from "testlib";
+import { assertEquals, describe, it } from "testlib";
 import { parseArgsDummy } from "./parseArgs.ts";
-import { booleanFlag, floatFlag, intFlag, stringFlag } from "./flagParsers.ts";
-import { Flag, FlagParser } from "./flags.ts";
-
-export function makeFlag<T>(name: string, parser: FlagParser<T>): Flag<T> {
-  return {
-    slugs: [name],
-    description: `your argument named ${name}`,
-    shortcuts: [],
-    required: false,
-    default: undefined,
-    parser,
-  };
-}
-
-const _sampleFlagset = {
-  one: makeFlag<string>("one", stringFlag),
-  dos: makeFlag<string>("dos", stringFlag),
-  three: makeFlag<boolean>("three", booleanFlag),
-  four: makeFlag<number>("four", floatFlag),
-  cinco: makeFlag<number>("cinco", intFlag),
-};
-
-// so the question is, can I type this more strongly
-const _typecheck1: Record<string, Flag<unknown>> = _sampleFlagset;
-
-export type HaveTo = Required<Flag<string>>;
-
-type _flagsetPropnames = keyof typeof _sampleFlagset;
-
-type ParserPropTypes<Flagset> = {
-  [Prop in keyof Flagset]: boolean;
-};
-
-type _flagsetProptypes = ParserPropTypes<typeof _sampleFlagset>;
-
-// assertType()
 
 describe("basic parser", () => {
   it("does", () => {
