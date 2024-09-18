28cc170a6bba9c69cf8a95b5e60b6c9d7cfa25bb
Brad Olson <brad.olson@movedbylight.com>
2024-09-18 12:48:24 -0400

    style: pretty up test table

--- experiments/simple1/parsers.test.ts
+++ experiments/simple1/parsers.test.ts
@@ -5,43 +5,23 @@ import { booleanFlag, negatedFlag, stringFlag } from "./parsers.ts";
 import type { FlagParser } from "./flags.ts";
 
 type testCase1<T> = [FlagParser<T>, T | undefined, string[], string[]];
+
+// deno-fmt-ignore  (to keep the table concise)
 const testTable1: Record<string, testCase1<any>> = {
+
   //===== BOOLEAN TYPE FLAGS
   "boolean flags don't need extra args": [booleanFlag, true, [], []],
-  "boolean flags don't consume args": [booleanFlag, true, ["a", "b"], [
-    "a",
-    "b",
-  ]],
+  "boolean flags don't consume args": [booleanFlag, true, ["a", "b"], [ "a", "b", ]],
   "negatable flags don't need extra args": [negatedFlag, false, [], []],
-  "negatable flags don't consume args": [negatedFlag, false, ["a", "b"], [
-    "a",
-    "b",
-  ]],
+  "negatable flags don't consume args": [negatedFlag, false, ["a", "b"], [ "a", "b", ]],
+
   //===== SIMPLE STRING FLAGS
   "string flags need one argument": [stringFlag, undefined, [], []],
-  "string flags consume one argument": [stringFlag, "tree", [
-    "tree",
-    "mountain",
-    "apple",
-  ], ["mountain", "apple"]],
-  "string flags can consume one final argument": [
-    stringFlag,
-    "tree",
-    ["tree"],
-    [],
-  ],
-  "string flags can consume long, quoted arguments": [
-    // whether the cli parser supports them depends on the quality of
-    // the shell-runtime interface
-    stringFlag,
-    "tree with long long branches",
-    [
-      "tree with long long branches",
-      "mountain",
-      "apple",
-    ],
-    ["mountain", "apple"],
-  ],
+  "string flags consume one argument": [stringFlag, "tree", [ "tree", "mountain", "apple", ], ["mountain", "apple"]],
+  "string flags can consume one final argument": [ stringFlag, "tree", ["tree"], [], ],
+  // whether the cli parser supports them depends on the quality of
+  // the shell-runtime interface
+  "string flags can consume long, quoted arguments": [  stringFlag, "tree with long long branches", [ "tree with long long branches", "mountain", "apple", ], ["mountain", "apple"], ],
 };
 
 describe("test flag mini parsers", () => {
