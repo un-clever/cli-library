830348ba8fcd63e129ac3844205111466a8f919a
Brad Olson <brad.olson@movedbylight.com>
2024-09-21 18:45:03 -0400

    wip: spike properly parses positional args and dashdash

--- experiments/simple2/spikes/cmd_parser_1.test.ts
+++ experiments/simple2/spikes/cmd_parser_1.test.ts
@@ -22,11 +22,16 @@ const flagsetJPs = {};
 
 // deno-fmt-ignore  (to keep the table concise)
 const flagsetJPcases = [
+  // no dashdash
   {desc: "no args", input: [], args: [], dashdash: []},
   {desc: "one arg", input: ["a"], args: ["a"], dashdash: []},
   {desc: "two args", input: ["a", "b"], args: ["a", "b"], dashdash: []},
   {desc: "three args", input: ["a", "b", "c"], args: ["a", "b", "c"], dashdash: []},
   {desc: "four args", input: ["a", "b", "c", "d"], args: ["a", "b", "c", "d"], dashdash: []},
+  // with a dashdash
+  {desc: "initial dashdash", input: ["--","a", "b",  "c", "d"], args: [] , dashdash: ["a", "b","c", "d"]},
+  {desc: "middle dashdash", input: ["a", "b", "--", "c", "d"], args: ["a", "b"] , dashdash: ["c", "d"]},
+  {desc: "terminal dashdash", input: ["a", "b", "c", "d", "--"], args: ["a", "b", "c", "d"] , dashdash: []},
 ];
 
 function parse(flagset: any, rawargs: string[]): CliArgs<{ one?: string }> {
@@ -41,7 +46,7 @@ function parse(flagset: any, rawargs: string[]): CliArgs<{ one?: string }> {
     // console.debug(tail, tail.length, args, args.length);
     const arg1 = tail.shift() as string;
     // console.debug(tail, arg1, args);
-    if (arg1 === "---") {
+    if (arg1 === "--") {
       dashdash = tail;
       tail = [];
     } else if (arg1.startsWith("-")) args.push("flag");
