cb8672ebe52004fd0e661850fd5c29e0ea161a12
Brad Olson <brad.olson@movedbylight.com>
2024-09-24 14:12:54 -0400

    begin(red): experiment to pass index into parser

--- experiments/simple2/flagParsers.ts
+++ experiments/simple2/flagParsers.ts
@@ -1,13 +1,12 @@
 import type { FlagParser } from "./types.ts";
 
+const FailedParse = [undefined, 0];
+
 /**
  * A normal boolean flag, false by default, true if it's present: e.g. --wrap.
  *
  * If you need fancier, default-true (negatable) flags (e.g. --no-wrap). See
  * falseFlag() below.
- *
- * @param args
- * @returns
  */
 export const booleanFlag: FlagParser<boolean> = {
   parse(_: string[]) {
@@ -23,41 +22,46 @@ export const booleanFlag: FlagParser<boolean> = {
  * Negatable flags can confuse users and developers, beware. True by default,
  * false if they're present.
  *
- * If you must have a default-true flag with a --no-destroy, either rephrase it
- * or, use this flag type. You could auto-populate it in your command factory
- * @param args
- * @returns
+ * IMO, If you must have a default-true flag with a --no-destroy, beset to
+ * rephrase it if you can. But, here's a helpful type if you want it.
  */
-export const negatedFlag: FlagParser<boolean> = (args: string[]) => {
-  return { value: false, tail: args };
+export const negatedFlag: FlagParser<boolean> = {
+  parse(_: string[]) {
+    return [false, 0]; // negated flags are true by default, false if present
+  },
+  default: true,
 };
-negatedFlag.default = true; // if it's not there, the flag is true
 
-export const stringFlag: FlagParser<string> = (args: string[]) => {
-  if (args.length < 1) return { tail: args };
-  return { value: args[0], tail: args.slice(1) };
+/**
+ * Parse a string flag
+ */
+export const stringFlag: FlagParser<string> = {
+  parse(args: string[]) {
+    if (args.length < 1) return FailedParse;
+    return [args[0], 1];
+  },
 };
 
 /**
  * Parse a decimal floating point number into a JavaScript number
- * @param args
- * @returns
  */
-export const floatFlag: FlagParser<number> = (args: string[]) => {
-  const value = parseFloat(args[0]);
-  if (isNaN(value)) return { tail: args };
-  return { value, tail: args.slice(1) };
+export const floatFlag: FlagParser<number> = {
+  parse(args: string[]) {
+    const value = parseFloat(args[0]);
+    if (isNaN(value)) return FailedParse;
+    return [value, 1];
+  },
 };
 
 /**
  * Parse a decimal integer into a JavaScript number
- * @param args
- * @returns
  */
-export const intFlag: FlagParser<number> = (args: string[]) => {
-  const value = parseInt(args[0]);
-  if (isNaN(value)) return { tail: args };
-  return { value, tail: args.slice(1) };
+export const intFlag: FlagParser<number> = {
+  parse(args: string[]) {
+    const value = parseInt(args[0]);
+    if (isNaN(value)) return FailedParse;
+    return [value, 1];
+  },
 };
 
 /**
@@ -90,13 +94,15 @@ export const intFlag: FlagParser<number> = (args: string[]) => {
  * @param args a string version of a date, preferably YYYY-MM-DD numeric
  * @returns a UTC timestamp with zeroed timefields
  */
-export const dateFlag: FlagParser<Date> = (args: string[]) => {
-  const test = new Date(args[0]);
-  if (test + "" === "Invalid Date") return { tail: args };
-  const value = new Date(
-    test.getUTCFullYear(),
-    test.getUTCMonth(),
-    test.getUTCDate(),
-  );
-  return { value, tail: args.slice(1) };
+export const dateFlag: FlagParser<Date> = {
+  parse(args: string[]) {
+    const test = new Date(args[0]);
+    if (test + "" === "Invalid Date") return FailedParse;
+    const value = new Date(
+      test.getUTCFullYear(),
+      test.getUTCMonth(),
+      test.getUTCDate(),
+    );
+    return [value, 1];
+  },
 };
