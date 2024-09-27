376214f641e4cc637e85861f63f77ca3532b3e05
Brad Olson <brad.olson@movedbylight.com>
2024-09-27 14:04:32 -0400

    test(green): move flagset parsing test to a separate file

--- vNext/tests/command.test.ts
+++ vNext/tests/command.test.ts
@@ -1,5 +1,5 @@
 // deno-lint-ignore-file no-unused-vars
-import { command, FlagsParser, runCommand } from "../command.ts";
+import { command, runCommand } from "../command.ts";
 import { getTestFlagset } from "./testUtils.ts";
 import type {
   CliArgs,
@@ -8,38 +8,11 @@ import type {
   FlagsetParseFn,
   StringWrite,
 } from "../types.ts";
-import {
-  assertEquals,
-  assertThrows,
-  assertType,
-  describe,
-  type IsExact,
-  it,
-} from "testlib";
+import { assertEquals, assertType, describe, type IsExact, it } from "testlib";
 import { Buffer } from "@std/io";
-import {
-  booleanFlagset,
-  booleanFlagsetCases,
-  dashDashCases,
-  defaultingNumericFlagset,
-  defaultingNumericFlagsetCases,
-  defaultingStringFlagset,
-  defaultingStringFlagsetCases,
-  optionalNumericFlagset,
-  optionalNumericFlagsetCases,
-  optionalStringFlagset,
-  optionalStringFlagsetCases,
-  requiredNumericFlagset,
-  requiredNumericFlagsetCases,
-  requiredStringFlagset,
-  requiredStringFlagsetCases,
-  simpleArgsCases,
-} from "./testData.ts";
-import { testmanyArgExamples } from "./testUtils.ts";
-import { testmanyFlagsetExamples } from "./testUtils.ts";
 
 const testFlagset = getTestFlagset();
-const { one, dos, three, four, cinco } = testFlagset;
+const { one } = testFlagset;
 
 describe("we can make a simple command", () => {
   type CommandType = { one?: string };
@@ -74,90 +47,4 @@ describe("we can make a simple command", () => {
     const decoder = new TextDecoder(); //...and here's grabbing that output
     assertEquals(JSON.parse(decoder.decode(output.bytes())), expectedParams);
   });
-});
-
-describe("we can parse simple positional arguments", () => {
-  const parser = new FlagsParser<unknown>({});
-  const parse = (args: string[]) => parser.parse(args);
-  testmanyArgExamples(parse, simpleArgsCases);
-});
-
-describe("we can parse positional args with a --", () => {
-  const parser = new FlagsParser<unknown>({});
-  const parse = (args: string[]) => parser.parse(args);
-  testmanyArgExamples(parse, dashDashCases);
-});
-
-describe("we can disallow -- in the args", () => {
-  const parser = new FlagsParser<unknown>({}, false);
-  const parse = (args: string[]) => parser.parse(args);
-  for (const c of dashDashCases) {
-    it("can always prohibit dashdash in the args", () => {
-      assertThrows(() => parse(c.raw));
-    });
-  }
-});
-
-function makeParseFn<VV>(fs: Flagset<VV>): FlagsetParseFn<VV> {
-  const parser = new FlagsParser(fs);
-  return (args: string[]) => parser.parse(args);
-}
-
-describe("we can parse boolean (default-false) flags", () => {
-  const parse = makeParseFn(booleanFlagset);
-  testmanyFlagsetExamples("booleanFlag", parse, booleanFlagsetCases);
-});
-
-describe("we can parse required string flags", () => {
-  const parse = makeParseFn(requiredStringFlagset);
-  testmanyFlagsetExamples(
-    "required string flag",
-    parse,
-    requiredStringFlagsetCases,
-  );
-});
-
-describe("we can parse defaulting string flags", () => {
-  const parse = makeParseFn(defaultingStringFlagset);
-  testmanyFlagsetExamples(
-    "optional string flag with default",
-    parse,
-    defaultingStringFlagsetCases,
-  );
-});
-
-describe("we can parse optional string flags", () => {
-  const parse = makeParseFn(optionalStringFlagset);
-  testmanyFlagsetExamples(
-    "optional string flag without default",
-    parse,
-    optionalStringFlagsetCases,
-  );
-});
-
-describe(" we can parse required numeric flags", () => {
-  const parse = makeParseFn(requiredNumericFlagset);
-  testmanyFlagsetExamples(
-    "required numeric flag without default",
-    parse,
-    requiredNumericFlagsetCases,
-  );
-});
-
-describe(" we can parse defaulting numeric flags", () => {
-  const parse = makeParseFn(defaultingNumericFlagset);
-  testmanyFlagsetExamples(
-    "required string flag with default",
-    parse,
-    defaultingNumericFlagsetCases,
-  );
-});
-
-describe(" we can parse optional numeric flags", () => {
-  const parse = makeParseFn(optionalNumericFlagset);
-  testmanyFlagsetExamples(
-    "optional numeric flag without default",
-    parse,
-    optionalNumericFlagsetCases,
-  );
 });
