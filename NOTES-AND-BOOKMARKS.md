561b4cae7aee7de9235d154e6d3ec6794a5b21fb
Brad Olson <brad.olson@movedbylight.com>
2024-09-30 15:29:08 -0400

    begin: start work on enum flag

--- NOTES-AND-BOOKMARKS.md
+++ NOTES-AND-BOOKMARKS.md
@@ -1,6 +1,6 @@
 
 30 Sep 2024: ideas from pocketbase tools
 
-4. COULD DO: add strings of array to flag types in unclever
+4. COULD DO: add strings of array to flag types in unclever STARTED
 5. COULD DO: add unclever multicommand
 6. COULD DO: add config source intercepters (ordered) to unclever, e.g. envvars, config file, should be async and ordered.
---
+++ src/extras/enumFlag.ts
@@ -0,0 +1,30 @@
+// enum flags are limited sets of strings
+
+import { FailedParse } from "../flags.ts";
+import type { FlagtypeDef } from "../types.ts";
+
+/**
+ * Extract a union type of the strings making up a string array constant.
+ */
+export type StringsToEnums<SS extends string[]> = SS[number];
+
+/**
+ * Create a flag type (parser) from an array of legal string values
+ * @param legalValues
+ * @returns
+ */
+export function makeEnumFlag<SS extends string[]>(legalValues: SS) {
+  type EE = StringsToEnums<SS>;
+  const flagParser: FlagtypeDef<EE> = {
+    parse: (i: number, args: string[]) => {
+      const n = 1;
+      const trialValue = args[i];
+      if (legalValues.includes(trialValue)) {
+        const value = trialValue as EE;
+        return { n, value };
+      }
+      return FailedParse;
+    },
+  };
+  return flagParser;
+}
