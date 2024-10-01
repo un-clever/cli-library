6808487b48ed9ce95e64e5a6a0a6cd147a948864
Brad Olson <brad.olson@movedbylight.com>
2024-10-01 18:13:34 -0400

    feat(green): add enumFlag parser

--- src/extras/enumFlag.test.ts
+++ src/extras/enumFlag.test.ts
@@ -1,9 +1,12 @@
 // deno-lint-ignore-file no-unused-vars
-import { assertEquals, assertType, describe, IsExact, it } from "testlib";
+import { assertEquals, assertType, describe, type IsExact, it } from "testlib";
 import type { FlagsetParseFn, FlagtypeDef, RequiredFlag } from "../types.ts";
-import { makeEnumFlag, StringArrayElements } from "./enumFlag.ts";
+import { makeEnumFlag, type StringArrayElements } from "./enumFlag.ts";
 import { required } from "../flags.ts";
-import { FlagsetExample, testmanyFlagsetExamples } from "../tests/testUtils.ts";
+import {
+  type FlagsetExample,
+  testmanyFlagsetExamples,
+} from "../tests/testUtils.ts";
 import { fuzzedExample } from "../tests/testData.ts";
 import { getFlagsetHelp, getFlagsetParser } from "../flagset.ts";
 
@@ -13,17 +16,16 @@ type flagTestCase<T> = [
   T | undefined, // expected parse result
   number, // number of args consumed
 ];
+const legalValues = ["one", "two", "three", "four"] as const;
+type TlegalValues = "one" | "two" | "three" | "four";
+
+assertType<
+  IsExact<StringArrayElements<typeof legalValues>, TlegalValues>
+>(true);
+
+const flag1to4: FlagtypeDef<TlegalValues> = makeEnumFlag(legalValues);
 
 describe("the enumFlag defines flag parses that accept only certain strings", () => {
-  const legalValues = ["one", "two", "three", "four"] as const;
-  type TlegalValues = "one" | "two" | "three" | "four";
-
-  assertType<
-    IsExact<StringArrayElements<typeof legalValues>, TlegalValues>
-  >(true);
-
-  const flag1to4: FlagtypeDef<TlegalValues> = makeEnumFlag(legalValues);
-
   const enumFlagExamples: flagTestCase<TlegalValues>[] = [
     [flag1to4, ["one"], "one", 1],
     [flag1to4, ["two"], "two", 1],
@@ -44,27 +46,35 @@ describe("the enumFlag defines flag parses that accept only certain strings", ()
       }
     }
   });
+});
 
+describe("the enumFlag defines flag parses that accept only certain strings", () => {
   type enumReturn = { code: TlegalValues };
   const enumFlags = {
     code: required("code", "some code for a thing", flag1to4, "four"),
   };
   const enumFlagsParser = getFlagsetParser(enumFlags);
+
+  // deno-fmt-ignore  (to keep the table concise)
   const enumFlagsExamples: FlagsetExample<enumReturn>[] = [
-    {
-      raw: [],
-      parsed: { args: [], flags: { code: "four" }, dashdash: [] },
-    },
+    ...fuzzedExample<enumReturn>({ raw: [], parsed: { args: [], flags: { code: "four" }, dashdash: [] }}),
+    ...fuzzedExample<enumReturn>({ raw: ["--code", "one"], parsed: { args: [], flags: { code: "one" }, dashdash: [] }}),
+    ...fuzzedExample<enumReturn>({ raw: ["--code", "two"], parsed: { args: [], flags: { code: "two" }, dashdash: [] }}),
+    ...fuzzedExample<enumReturn>({ raw: ["--code", "three"], parsed: { args: [], flags: { code: "three" }, dashdash: [] }}),
+    ...fuzzedExample<enumReturn>({ raw: ["--code", "four"], parsed: { args: [], flags: { code: "four" }, dashdash: [] }}),
+    ...fuzzedExample<enumReturn>({ raw: ["--code", "five"], parsed: new Error()}),
+    ...fuzzedExample<enumReturn>({ raw: ["--code"], parsed: new Error()}),
   ];
 
-  it("defaults work correctly", () => {
-    testmanyFlagsetExamples(
-      "enumFlag Required with Default",
-      enumFlagsParser,
-      [{
-        raw: [],
-        parsed: { args: [], flags: { code: "four" }, dashdash: [] },
-      }],
-    );
-  });
+  testmanyFlagsetExamples(
+    "enumFlag Required with Default",
+    enumFlagsParser,
+    enumFlagsExamples,
+    // [
+    //   {
+    //     raw: [],
+    //     parsed: { args: [], flags: { code: "four" }, dashdash: [] },
+    //   },
+    // ],
+  );
 });
